28caf46f22008216114a50ff8b322c33ac32f559
Fabian Ackle <fabian@budo-team.ch>
2021-02-23 16:58:20 +0100

    Würfe in Gruppen unterteilt. 30 Sekunden Pause zwischen den Gruppen / 12 Sekundenzwischen den Würfen. Namen auf Japanisch, Infos auf Englisch

--- main.py
+++ main.py
@@ -5,53 +5,74 @@ import subprocess
 import time
 
 THROWS = [
-    'De-ashi-barai',
-    'Hiza-guruma',
-    'Sasae-tsurikomi-ashi',
-    'Uki-goshi',
-    'Osoto-gari',
-    'O-goshi',
-    'Ouchi-gari',
-    'Seoi-nage',
-    'Kosoto-gari',
-    'Kouchi-gari',
-    'Koshi-guruma',
-    'Tsurikomi-goshi',
-    'Okuri-ashi-barai',
-    'Tai-otoshi',
-    'Harai-goshi',
-    'Uchi-mata',
-    'Kosoto-gake',
-    'Tsuri-goshi',
-    'Yoko-otoshi',
-    'Ashi-guruma',
-    'Hane-goshi',
-    'Harai-tsurikomi-ashi',
-    'Tomoe-nage',
-    'Kata-guruma',
-    'Sumi-gaeshi',
-    'Tani-otoshi',
-    'Hane-makikomi',
-    'Sukui-nage',
-    'Utsuri-goshi',
-    'O-guruma',
-    'Soto-makikomi',
-    'Uki-otoshi',
-    'Osoto-guruma',
-    'Uki-waza',
-    'Yoko-wakare',
-    'Yoko-guruma',
-    'Ushiro-goshi',
-    'Ura-nage',
-    'Sumi-otoshi',
-    'Yoko-gake'
+    [
+        'De-ashi-barai',
+        'Hiza-guruma',
+        'Sasae-tsurikomi-ashi',
+        'Uki-goshi',
+        'Osoto-gari',
+        'O-goshi',
+        'Ouchi-gari',
+        'Seoi-nage'
+    ],
+    [
+        'Kosoto-gari',
+        'Kouchi-gari',
+        'Koshi-guruma',
+        'Tsurikomi-goshi',
+        'Okuri-ashi-barai',
+        'Tai-otoshi',
+        'Harai-goshi',
+        'Uchi-mata'
+    ],
+    [
+        'Kosoto-gake',
+        'Tsuri-goshi',
+        'Yoko-otoshi',
+        'Ashi-guruma',
+        'Hane-goshi',
+        'Harai-tsurikomi-ashi',
+        'Tomoe-nage',
+        'Kata-guruma'
+    ],
+    [
+        'Sumi-gaeshi',
+        'Tani-otoshi',
+        'Hane-maki-komi',
+        'Sukui-nage',
+        'Utsuri-goshi',
+        'O-guruma',
+        'Soto-maki-komi',
+        'Uki-otoshi'
+    ],
+    [
+        'Osoto-guruma',
+        'Uki-waza',
+        'Yoko-wakare',
+        'Yoko-guruma',
+        'Ushiro-goshi',
+        'Ura-nage',
+        'Sumi-otoshi',
+        'Yoko-gake'
+    ]
 ]
 
-def play(msg):
-    tts = gTTS(msg.replace('-',' '), lang='ja')
+def play(msg, lg, speed):
+    tts = gTTS(msg.replace('-',' '), lang=lg, slow=speed)
     tts.save(f'sound/{msg}.mp3')
     subprocess.call(["ffplay", "-nodisp", "-autoexit", f"sound/{msg}.mp3"])
 
-for throw in THROWS:
-    play(throw)
-    time.sleep(10)
+play('Start in 10 seconds', 'en', False)
+time.sleep(10)
+play('let\'s go', 'en', False)
+
+for group in THROWS:
+    for throw in group:
+        play(throw, 'ja', True)
+        time.sleep(1)
+
+    play("30 seconds break", 'en', False)
+    time.sleep(25)
+    play("It continues!", 'en', False)
+
+
